0a76637cb7e4224d23825be98772fd4ffc1784f6
Ignacio-1712 <ignaciopereyra912@gmail.com>
2025-12-20 10:22:58 -0300

    Auto gestor de la base
    Desplazamiento de hosting

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://TU_PROYECTO.supabase.cohttps://vuqrdulufxvvufhlsxqx.supabase.co",
+  "sb_publishable_hDJqmkRa4pTk-CXnnY61tA__6Y6HGYM"
+);

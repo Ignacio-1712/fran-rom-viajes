dfe88a9e680e603a4049a1db9337976f2960422f
Ignacio-1712 <ignaciopereyra912@gmail.com>
2025-12-20 10:58:39 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://TU_PROYECTO.supabase.cohttps://vuqrdulufxvvufhlsxqx.supabase.co",
-  "sb_publishable_hDJqmkRa4pTk-CXnnY61tA__6Y6HGYM"
-);
